045b6fbecc0bfd0ac9967984d9b74dbceae7b3a7
Plamen Damyanov <dupzzy37@gmail.com>
2023-01-27 17:24:00 +0200

    Library.sol
    Updated variable names
    Added comments explaining code

--- Library.sol
+++ Library.sol
@@ -7,15 +7,40 @@ import "./Ownable.sol";
 contract Library is Ownable {
 
     struct Book {
-        string title;
-        uint32 availableCopies;
-        address[] historyOfBorrowings;
+        uint32 availableCopies;     // How much copies do we have from the book
+        address[] borrowingLedger;  // Who has borrowed the book
     }
 
-    string[] totalTitles;
+    /*  
+    *   Users should be able to see the available books
+    *   If I declare my mapping as public, getter method will be auto-generated.
+    *   The problem with that is remix requires you to input key, to get a value from this getter.
+    *   What I want to do is display all the available books in one go.
+    *   To tackle this I'm creating a string array, which I update when the following conditions are met:
+    *   1: If a book is added, it's title will be added here.
+    *   1.2: If the title exists in the string array, it is not added again
+    *   3: When borrowing a book, if the availableCopies get down to 0, array is updated by deleting the book title from it, leaving an empty string
+    */
+    string[] public availableTitles;
 
-    mapping(bytes32 => Book) private books;
-    mapping(address => bytes32[]) private usersCurrentlyBorrowedBooks;
+    /*
+    *   The mapping books, cotains the information of the unique titles added.
+    *   The logic here is as follows:
+    *   1: We receive a book by string (bookName / title)
+    *   2: I convert that name to bytes32 via keccak256 to get the id for that title
+    *   3: I use the ID to access the title details
+    *   3.1: I increment the amount of availableCopies
+    *   3.2: When someone borrows a book, his address is saved to borrowingLedger
+    */
+    mapping(bytes32 => Book) public books;
+
+    /*
+    *   A user should not borrow more than one copy of a book at a time.
+    *   The way I tackle this problem, if I understood it correctly is to have a mapping, that holds the ID's of all books that a user has borrowed
+    *   The key to accessing the value is the address of the sender
+    *   The value is array holding bytes32 ID's that are generated from having a book title go through keccak256
+    */
+    mapping(address => bytes32[]) private borrowedTitles;
 
     // Function that check if string is present in array
     function stringExists(string memory target, string[] memory arr) internal pure returns (bool) {
@@ -29,79 +54,68 @@ contract Library is Ownable {
         return false;
     }
  
-    function addBooks(string memory _bookName) public onlyOwner {
-        if(!stringExists(_bookName, totalTitles)) 
-            totalTitles.push(_bookName);
-        bytes32 id = keccak256(abi.encodePacked(_bookName));
+    function addBooks(string memory _title) public onlyOwner {
+        if(!stringExists(_title, availableTitles)) 
+            availableTitles.push(_title);
+
+        bytes32 id = keccak256(abi.encodePacked(_title));
         books[id].availableCopies++;
-        books[id].title = _bookName;
     }
 
-    function checkIfUserHasBorrowedTitle(string memory _bookName) private view returns(bool) {
-        bytes32 id = keccak256(abi.encodePacked(_bookName));
-        bytes32[] memory borrowedTitles = usersCurrentlyBorrowedBooks[msg.sender];  // Getting the current user borrowed titles
-        uint32 borrowedTitlesLength = uint32(borrowedTitles.length);                // Getting the amount of borrowed titles
+    function checkIfUserHasBorrowedTitle(string memory _title) private view returns(bool) {
+        bytes32 id = keccak256(abi.encodePacked(_title));
+        uint32 borrowedTitlesAmount = uint32(borrowedTitles[msg.sender].length);
 
-        // For loop, checking if the title we are gonna borrow is already borrowed by the user
-        bool isBorrowed = false;
-        for (uint32 i = 0; i < borrowedTitlesLength; i++) {
-            if(borrowedTitles[i] == id)
-                isBorrowed = true;
+        for (uint32 i = 0; i < borrowedTitlesAmount; i++) {
+            if(borrowedTitles[msg.sender][i] == id)
+                return true;
         }
 
-        return isBorrowed;
+        return false;
     }
 
-    function borrowBooks(string memory _bookName) public {
-        require(stringExists(_bookName, totalTitles), "There is no such book title");
-        bytes32 id = keccak256(abi.encodePacked(_bookName));
-        require(books[id].availableCopies != 0, "There are no more available copies of this book");
+    function borrowBook(string memory _title) public {
+        require(stringExists(_title, availableTitles), "No available copies of this book");
+        bytes32 id = keccak256(abi.encodePacked(_title));
 
-        require(!checkIfUserHasBorrowedTitle(_bookName), "User has already borrowed this title");
-        usersCurrentlyBorrowedBooks[msg.sender].push(id); // In case user hasn't borrow this title, we add it to his list of borrowed titles
-
-        books[id].historyOfBorrowings.push(msg.sender);    // Add sender to the history of borrowings of the unique title
-        books[id].availableCopies--;                       // Decrement the amount of available copies of the unique title 
+        require(!checkIfUserHasBorrowedTitle(_title), "User has already borrowed this title");
+        borrowedTitles[msg.sender].push(id);   // In case the require passed, we add the _title ID to the sender borrowedTitles
+        books[id].borrowingLedger.push(msg.sender);     // Add sender to the history of borrowings of the unique title
+        books[id].availableCopies--;                        // Decrement the amount of available copies of the unique title 
 
         // If there are no available copies of the book, we must remove it from totalTiles, since there are no copies to borrow from
         if(books[id].availableCopies == 0) {
-            uint32 totalTitlesLength = uint32(totalTitles.length);
-
-            for(uint32 i = 0; i < totalTitlesLength; i++) {
-                if(keccak256(abi.encodePacked(totalTitles[i])) == id)
-                    delete totalTitles[i];
+            uint32 availableTitlesLength = uint32(availableTitles.length);
+            for(uint32 i = 0; i < availableTitlesLength; i++) {
+                if(keccak256(abi.encodePacked(availableTitles[i])) == id)
+                    delete availableTitles[i];
             }
         }  
     }
 
-    function returnBook(string memory _bookName) public {
-        require(checkIfUserHasBorrowedTitle(_bookName), "User has not borrowed this title, this cannot return it");
+    function returnBook(string memory _title) public {
+        require(checkIfUserHasBorrowedTitle(_title), "User has not borrowed this title, thus cannot return it");
+        bytes32 id = keccak256(abi.encodePacked(_title));
 
-        // Getting some id of the title, and the msg.sender borrowed titles list
-        bytes32 id = keccak256(abi.encodePacked(_bookName));
-        bytes32[] memory borrowedTitles = usersCurrentlyBorrowedBooks[msg.sender];
-        uint32 borrowedTitlesLength = uint32(borrowedTitles.length); 
-
+        uint32 borrowedTitlesLength = uint32(borrowedTitles[msg.sender].length); 
         // This for loop will remove the borrowed title from the list of borrowed titles of the msg.sender
-        for(uint32 i = 0; i < borrowedTitlesLength; i++)
-            if(borrowedTitles[i] == id)
-                delete (usersCurrentlyBorrowedBooks[msg.sender])[i];
+        for(uint32 i = 0; i < borrowedTitlesLength; i++) {
+            if(borrowedTitles[msg.sender][i] == id) {
+                delete (borrowedTitles[msg.sender])[i];
+            }
+        }
 
-        // After we've remove the title from the msg.sender borrowed titles list, we need to increment the amount 
+        // After we've removed the title from the msg.sender borrowed titles list, we need to increment the amount in the library (books mapping)
         books[id].availableCopies++;
 
-        // Add book to totalTitles in case there are no other copies, and totalTitles doesn't contain the title anymore
-        if(!stringExists(_bookName, totalTitles)) 
-            totalTitles.push(_bookName);
+        // Add book to availableTitles in case there are no other copies, and availableTitles doesn't contain the title anymore
+        if(!stringExists(_title, availableTitles)) {
+            availableTitles.push(_title);
+        }
     }
 
-    function availableBooks() public view  returns (string[] memory) {
-        require(totalTitles.length > 0, "No available books");
-        return totalTitles;
-    }
-
-    function listBorrowings(string memory _bookName) public view returns (address[] memory) {
-        bytes32 id = keccak256(abi.encodePacked(_bookName));
-        return books[id].historyOfBorrowings;
+    function listBorrowings(string memory _title) public view returns (address[] memory) {
+        bytes32 id = keccak256(abi.encodePacked(_title));
+        return books[id].borrowingLedger;
     }
 }
