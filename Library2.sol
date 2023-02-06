d439867c1cca178c3e167c15b058dbd4add7832b
Plamen Damyanov <dupzzy37@gmail.com>
2023-02-06 17:16:47 +0200

    Simplified version

--- Library2.sol
+++ Library2.sol
@@ -2,51 +2,42 @@
 pragma solidity ^0.8.0;
 pragma abicoder v2;
 
-import "./Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 contract Library is Ownable {
 
-    struct BookContext {
-        uint32 id;
+    struct Book {
         bool firstAdd;
+        uint32 availableCopies;     
+        string title; 
         address[] borrowingHistory;
         mapping(address => bool) currentBorrowers; 
     }
 
-    struct Book {
-        uint32 availableCopies;     
-        string title; 
-    }
-
-    // Our context will contain id, which we are gonna use to index our books array
-    // A boolean that will tell us if the owner has ever pushed such a book in the books array
-    // And borrowing ledger, that lets us know the history of borrowings of certain title
-    // And a mapping that tracks current borrowers, so they cannot borrow the same book again
-    mapping(bytes32 => BookContext) bookContext; 
-    uint32 uniqueTitles;    // Size of the books array
-    Book[] books;           // Our books array contains the available copies of the unique book and it's title
-
+    mapping(bytes32 => Book) public books; 
+    bytes32[] public iterator;           
+    
+    // Checking if such a book was ever added to the library
+    // There is a case where user is trying to borrow a book, and there are 0 left
+    // If there are 0 left, there are either borrowed or never added
+    // In case they are borrowed, user can expect them to be returned at some point
     modifier checkTitle(string memory _title) {
         bytes32 key = keccak256(abi.encodePacked(_title));
-        require(bookContext[key].firstAdd, "Library doesn't contain the specified book");
+        require(books[key].firstAdd == true, "Library doesn't contain the specified book");
         _;
     }
 
     function addBooks(string memory _title) public onlyOwner {
         bytes32 key = keccak256(abi.encodePacked(_title));
 
-        if(!bookContext[key].firstAdd) {
-            bookContext[key].id = uniqueTitles++;   // This is going to be the index for this title, e.g. they way we get books.availableCopies
-            bookContext[key].firstAdd = true;       // Making sure we wont rewrite something that is already pushed to the array
-
-            // Pushing this book title for the first time in the array, that is why available copies == 1
-            books.push(Book({
-                availableCopies: 1,
-                title: _title
-            }));
+        if(!books[key].firstAdd) {
+            books[key].title = _title;
+            books[key].availableCopies++;
+            books[key].firstAdd = true;      
+            iterator.push(key);
         } else {
-            books[bookContext[key].id].availableCopies++;
+            books[key].availableCopies++;
         }
 
         return;
@@ -54,51 +45,30 @@ contract Library is Ownable {
 
     function borrowBook(string memory _title) public checkTitle(_title) {
         bytes32 key = keccak256(abi.encodePacked(_title));
-        require(books[bookContext[key].id].availableCopies > 0, "There are no copies available from this book");
-        require(!bookContext[key].currentBorrowers[msg.sender], "The user has already borrowed this book");
+        require(books[key].availableCopies > 0, "There are no copies available from this book");
+        require(!books[key].currentBorrowers[msg.sender], "The user has already borrowed this book");
 
-        books[bookContext[key].id].availableCopies--;
-        bookContext[key].currentBorrowers[msg.sender] = true;
-        bookContext[key].borrowingHistory.push(msg.sender);
+        books[key].availableCopies--;
+        books[key].currentBorrowers[msg.sender] = true;
+        books[key].borrowingHistory.push(msg.sender);
 
         return;
     }
 
     function returnBook(string memory _title) public checkTitle(_title) {
         bytes32 key = keccak256(abi.encodePacked(_title));
-        require(bookContext[key].currentBorrowers[msg.sender], "User has not borrowed such a book");
+        require(books[key].currentBorrowers[msg.sender], "User has not borrowed such a book");
 
-        books[bookContext[key].id].availableCopies++;
-        bookContext[key].currentBorrowers[msg.sender] = false;
+        books[key].availableCopies++;
+        books[key].currentBorrowers[msg.sender] = false;
     }
     
-    function getAvailableTitles() public view returns (Book[] memory) {
-        require(uniqueTitles > 0, "Library is empty, need to add books first");
-        
-
-        uint32 retSize = 0;
-        for(uint32 i = 0; i < uniqueTitles; i++) {
-            if(books[i].availableCopies > 0) {
-                retSize++;
-            }
-        }
-
-        uint32 retPosition = 0;
-        Book[] memory ret = new Book[](retSize);
-        for(uint32 i = 0; i < uniqueTitles; i++) {
-            if(books[i].availableCopies > 0) {
-                ret[retPosition].title = books[i].title;
-                ret[retPosition].availableCopies = books[i].availableCopies;
-                retPosition++;
-            }
-        }
-
-        return ret;
+    function getNumberOfBooks() public view returns(uint) {
+        return iterator.length;
     }
 
     function getBorrowingHistory(string memory _title) public view checkTitle(_title) returns(address[] memory) {
         bytes32 key = keccak256(abi.encodePacked(_title));
-
-        return bookContext[key].borrowingHistory;
+        return books[key].borrowingHistory;
     }
 }
